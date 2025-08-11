43677579bf268d38a97055098b8e703a25554452
imharikrishna194 <harishharish97940@gmail.com>
2025-08-11 03:06:46 -0700

    try1

--- main.js
+++ main.js
@@ -1,22 +1,30 @@
+// Find Store Visibility
 const divBox = document.querySelector(".find-store")
 function clk() {
     divBox.style.display="block"
     divBox.style.transform= translateY("-100px");
-}
+};
 function clkRemove(){
     divBox.style.display="none"
-}
+};
 
+// Toggle mobile menu 
 const menuList = document.querySelector(".nav-links")
+//close menu when the nav link is clicked
+function menuClose(){
+    menuList.style.left="-100%"
+};
+//Open menu when the open button is clicked
 function menuOpenButton() { 
     menuList.style.left="-1%" ;
-}
+};
+//close menu when the close button is clicked
 function menuCloseButton(){
     
     menuList.style.left="-100%";
-}
+};
 
-
+// Size Button Function
 const sizeButtonSection2 = document.querySelector(".button-section2");
 const sizeButtonSection1 =  document.querySelector(".button-section1");
 
@@ -24,9 +32,9 @@ function viewButtonSection2() {
     sizeButtonSection2.style.display="block" ;
     sizeButtonSection1.style.display="none"
 
-}
+};
 function viewButtonSection1(){
     sizeButtonSection1.style.display="block" ;
     sizeButtonSection2.style.display="none" ;
     
-}
+};
